647b69869e80c4865afe3bf18ed9813f3bef6431
cafeway <bcaranja@gmail.com>
2024-02-14 14:17:52 +0300

    my model has name, description, start_date,end_date and rack rate as fillables and start and end date as protected cast fields

--- app/Models/Accomodations.php
+++ app/Models/Accomodations.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Accomodations extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name', 'description', 'start_date', 'end_date', 'rack_rate',
+    ];
+
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+    ];
 }
