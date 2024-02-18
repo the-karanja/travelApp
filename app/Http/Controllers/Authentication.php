1ee59534dd9529bbd846fdfae160f335fc0df9cb
cafeway <bcaranja@gmail.com>
2024-02-18 12:20:12 +0300

    i have added functions to handle get and post for all authentication  functions

--- app/Http/Controllers/Authentication.php
+++ app/Http/Controllers/Authentication.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class Authentication extends Controller
 {
-    //
+    //displays the login page for the travel agents
+    public function loginPage ()
+    {
+
+    }
+
+    // a post route for the login page to process login credentials
+
+    public function login ()
+    {
+
+    }
+
+    // this get request displays the register page for the travel agents
+
+    public function registerPage ()
+    {
+
+    }
+
+    // this post request creates a new account then redirects to the main page
+
+    public function Register ()
+    {
+
+    }
 }
